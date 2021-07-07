a41c9791793a7fde96620ceafbd44fc3b7a7e7c5
Bukola Omojowo <bomojowo@gmail.com>
2021-07-07 12:44:24 +0100

    Fetched from random dog pic api using both async/wait and .then function

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,67 @@
 import { useState } from "react";
 
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+// interface Joke {
+//   id: number;
+//   type: string;
+//   setup: string;
+//   punchline: string;
+// }
+
+interface DogPic {
+  message: string;
+  status: string;
 }
 
 function App() {
-  const [joke, setJoke] = useState<Joke>();
+  // const [joke, setJoke] = useState<Joke>();
+  const [pic, setPic] = useState<DogPic>();
 
-  const handleGetJoke = async () => {
-    const response = await fetch(
-      "https://official-joke-api.appspot.com/jokes/general/random"
-    );
-    const jsonBody: Joke[] = await response.json();
-    setJoke(jsonBody[0]);
-  };
-
-  // const handleGetJoke = () => {
-  //   fetch("https://official-joke-api.appspot.com/jokes/general/random")
-  //     .then((response) => response.json())
-  //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
+  // const handleGetJoke = async () => {
+  //   const response = await fetch(
+  //     "https://official-joke-api.appspot.com/jokes/general/random"
+  //   );
+  //   const jsonBody: Joke[] = await response.json();
+  //   setJoke(jsonBody[0]);
   // };
 
-  if (joke) {
+  const handleGetPic = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((jsonBody: DogPic) => setPic(jsonBody));
+  };
+
+  // const handleGetPic = async () => {
+  //   const response = await fetch(
+  //     "https://dog.ceo/api/breeds/image/random"
+  //   );
+  //   const jsonBody: DogPic = await response.json();
+  //   setPic(jsonBody)
+  // }
+
+  if (pic) {
+    console.log(pic.message)
     return (
       <div>
-        <h1>Joke app</h1>
-        <details>
-          <summary>{joke.setup}</summary>
+        <h1>Dog app</h1>
+        <p>
+          {/* <summary>{joke.setup}</summary>
           <p>{joke.punchline}</p>
-        </details>
+          <p>{joke.type}</p> */}
+          <img src={pic.message} alt="dog"/>
+        </p>
         <hr />
-        <button onClick={handleGetJoke}>Get another joke</button>
+        <button onClick={handleGetPic}>Get another pic</button>
       </div>
     );
   } else {
     return (
       <div>
-        <h1>Joke app</h1>
+        <h1>Dog app</h1>
         <p>
           Click the button to trigger a <code>fetch</code> that gets a random
-          joke from an API!
+          pic from an API!
         </p>
-        <button onClick={handleGetJoke}>Get joke</button>
+        <button onClick={handleGetPic}>Get pic</button>
       </div>
     );
   }
