35e8023352502db710f3023f22c36a21a14ea22b
Bukola Omojowo <bomojowo@gmail.com>
2021-07-07 16:46:14 +0100

    used array state to store multiple pictures

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ interface DogPic {
 
 function App() {
   // const [joke, setJoke] = useState<Joke>();
-  const [pic, setPic] = useState<DogPic>();
+  const [picArray, setPic] = useState<DogPic[]>([]);
 
   // const handleGetJoke = async () => {
   //   const response = await fetch(
@@ -24,47 +24,51 @@ function App() {
   //   setJoke(jsonBody[0]);
   // };
 
-  const handleGetPic = () => {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then((response) => response.json())
-      .then((jsonBody: DogPic) => setPic(jsonBody));
-  };
+  // const handleGetPic = () => {
+  //   fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then((response) => response.json())
+  //     .then((jsonBody: DogPic) => setPic(jsonBody));
+  // };
 
-  // const handleGetPic = async () => {
-  //   const response = await fetch(
-  //     "https://dog.ceo/api/breeds/image/random"
-  //   );
-  //   const jsonBody: DogPic = await response.json();
-  //   setPic(jsonBody)
-  // }
+   const handleGetPic = async () => {
+    const response = await fetch(
+      "https://dog.ceo/api/breeds/image/random"
+    );
+    const jsonBody: DogPic = await response.json();
+    setPic([...picArray, jsonBody])
+  }
 
-  if (pic) {
-    console.log(pic.message)
+  const storePics = () => {
+    handleGetPic();
+
+  }
+  //console.log(jsonBody)
+  console.log(picArray)
+//console.log(picArray)
+
+  
     return (
       <div>
+        {/* {setPic([])} */}
         <h1>Dog app</h1>
         <p>
           {/* <summary>{joke.setup}</summary>
           <p>{joke.punchline}</p>
           <p>{joke.type}</p> */}
-          <img src={pic.message} alt="dog"/>
+          {picArray.map((pic) => {
+            return <img src={pic.message} alt="dog pic" />
+            
+          })}
+          
+          {/* <img src={picArray.message} alt="dog"/> */}
         </p>
         <hr />
-        <button onClick={handleGetPic}>Get another pic</button>
+        <button onClick={storePics}>Get pic</button>
       </div>
     );
-  } else {
-    return (
-      <div>
-        <h1>Dog app</h1>
-        <p>
-          Click the button to trigger a <code>fetch</code> that gets a random
-          pic from an API!
-        </p>
-        <button onClick={handleGetPic}>Get pic</button>
-      </div>
-    );
+    
   }
-}
+
+
 
 export default App;
